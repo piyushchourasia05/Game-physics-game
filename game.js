4df06f8133d2b950a9d0c2d505fece21ed8e5647
piyushchourasia05 <78714786+piyushchourasia05@users.noreply.github.com>
2021-12-11 17:21:46 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,15 @@
+//Made By Piyush chourasia 20BCG10054
 var game;
 
-// groups containing crates and planets
+
 
 var crateGroup;
 var planetGroup;
 
-// a force reducer to let the simulation run smoothly
+
 
 var forceReducer = 0.005;
 
-// graphic object where to draw planet gravity area
 
 var gravityGraphics;
 
@@ -29,63 +29,53 @@ playGame.prototype = {
 	},
   	create: function(){
   		
-  		// adding groups
+  	
   		
   		crateGroup = game.add.group();
   		planetGroup = game.add.group();
-  		
-		// adding graphic objects
+  
 		
 		gravityGraphics = game.add.graphics(0, 0);
     		gravityGraphics.lineStyle(2,0xffffff,0.5);
-  		
-		// stage setup
+  	
 		
 		game.stage.backgroundColor = "#222222";
 		
-		// physics initialization
+
 		
 		game.physics.startSystem(Phaser.Physics.BOX2D);
     		
-		// adding a couple of planets. Arguments are:
-		// x position
-		// y position
-		// gravity radius
-		// gravity force
-		// graphic asset
+		
     		
 		addPlanet(180, 200, 250, 150, "planet");
     		addPlanet(570, 350, 400, 250, "bigplanet");
 		
-		// waiting for player input
-    		
+		
 		game.input.onDown.add(addCrate, this);
 	},
 	update: function(){
 	
-		// looping through all crates
-	
+		
 		for(var i=0;i<crateGroup.total;i++){	
 			var c = crateGroup.getChildAt(i);
 			
-			// looping through all planets
+			
 			
 			for(var j=0;j<planetGroup.total;j++){ 
 				var p = planetGroup.getChildAt(j);
 				
-				// calculating distance between the planet and the crate
 				
 				var distance = Phaser.Math.distance(c.x,c.y,p.x,p.y);
 				
-				// checking if the distance is less than gravity radius
+				
 				
 				if(distance<p.width/2+p.gravityRadius/2){
 					
-					// calculating angle between the planet and the crate
+					
 					
 					var angle = Phaser.Math.angleBetween(c.x,c.y,p.x,p.y);
 					
-					// add gravity force to the crate in the direction of planet center
+					
 					
 					c.body.applyForce(p.gravityForce*Math.cos(angle)*forceReducer,p.gravityForce*Math.sin(angle)*forceReducer);
 				}
@@ -94,7 +84,6 @@ playGame.prototype = {
 	}
 }
 
-// function to add a crate
 
 function addCrate(e){	
 	var crateSprite = game.add.sprite(e.x, e.y, "crate");
@@ -102,7 +91,6 @@ function addCrate(e){
     	game.physics.box2d.enable(crateSprite);
 }
 
-// function to add a planet
 
 function addPlanet(posX, posY, gravityRadius, gravityForce, asset){
 	var planet = game.add.sprite(posX, posY, asset);
@@ -112,7 +100,7 @@ function addPlanet(posX, posY, gravityRadius, gravityForce, asset){
 	game.physics.box2d.enable(planet);
 	planet.body.static = true;
 	
-	// look how I create a circular body
+	
 	
 	planet.body.setCircle(planet.width / 2);
 	gravityGraphics.drawCircle(planet.x, planet.y, planet.width+planet.gravityRadius);
